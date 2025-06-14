b01e969472adfbe3b0fd197dcfaf2e2803db052c
Arafat-Ahmad-Sheikh <asterisk287@gmail.com>
2025-06-14 14:08:41 +0530

    login and register

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,25 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
+import loginRegister from './components/loginRegister.vue'
+
+const session = ref(null)
+onMounted(() => {
+  supabase.auth.getSession().then(({ data }) => {
+    session.value = data.session
+  })
+  supabase.auth.onAuthStateChange((_, _session) => {
+    session.value = _session
+  })
+})
 </script>
 
 <template>
-  <HelloWorld />
+  <div>
+    <loginRegister v-if="!session" />
+    <div v-else>
+      <!-- Main dashboard or app goes here -->
+      <h1>Welcome!</h1>
+    </div>
+  </div>
 </template>
